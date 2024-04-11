e68b599387d07b5f3717258f8dbd8a417749b5df
lamdd <lamdd@tokyotechlab.com>
2024-04-11 11:04:32 +0700

    [test]

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -15,6 +15,10 @@ const count = ref(0)
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
   </div>
 
   <p>
